9f90d57fd3721f8b78c0e299b98984cc885741ba
roxyzc <roxyzc12@gmail.com>
2023-03-20 21:18:52 +0700

    update

--- ubah.php
+++ ubah.php
@@ -62,7 +62,7 @@ function is_select($var, $val)
             <form method="post" action="ubah.php" enctype="multipart/form-data">
                 <div class="input">
                     <label>Nama Barang</label>
-                    <input type="text" name="nama" value="<?php echo $data['nama']; ?>" />
+                    <input type="text" maxlength="30" name="nama" value="<?php echo $data['nama']; ?>" />
                 </div>
                 <div class="input">
                     <label>Kategori</label>
@@ -74,15 +74,15 @@ function is_select($var, $val)
                 </div>
                 <div class="input">
                     <label>Harga Jual</label>
-                    <input type="text" name="harga_jual" value="<?php echo $data['harga_jual']; ?>" />
+                    <input type="text" maxlength="12" name="harga_jual" value="<?php echo $data['harga_jual']; ?>" />
                 </div>
                 <div class="input">
                     <label>Harga Beli</label>
-                    <input type="text" name="harga_beli" value="<?php echo $data['harga_beli']; ?>" />
+                    <input type="text" maxlength="12" name="harga_beli" value="<?php echo $data['harga_beli']; ?>" />
                 </div>
                 <div class="input">
                     <label>Stok</label>
-                    <input type="text" name="stok" value="<?php echo $data['stok']; ?>" />
+                    <input type="text" maxlength="12" name="stok" value="<?php echo $data['stok']; ?>" />
                 </div>
                 <div class="input">
                     <label>File Gambar</label>
